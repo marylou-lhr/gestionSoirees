56d9834a8190788b3747dc3c3c5f360cc503117b
Marylou Lohier <mlohier001@iutbayonne.univ-pau.fr>
2025-04-05 16:24:26 +0200

    Fix des routes et modifications frontend

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,18 @@ use App\Http\Controllers\ReservationController;
 */
 //Routes autour des soirées
 Route::get('/soirees', [SoireeController::class, 'index']);
-Route::post('/soirees', [SoireeController::class, 'store']);
+Route::post('/soirees', [SoireeController::class, 'create']);
 Route::put('/soirees/{id}', [SoireeController::class, 'update']);
 Route::delete('/soirees/{id}', [SoireeController::class, 'destroy']);
 
 //Routes autour des goodies
 Route::get('/goodies', [GoodieController::class, 'index']);
-Route::post('/goodies', [GoodieController::class, 'store']);
+Route::post('/goodies', [GoodieController::class, 'create']);
 Route::put('/goodies/{id}', [GoodieController::class, 'update']);
 Route::delete('/goodies/{id}', [GoodieController::class, 'destroy']);
 
 //Routes autour des réservations
 Route::get('/reservations', [ReservationController::class, 'index']);
-Route::post('/reservations', [ReservationController::class, 'store']);
+Route::post('/reservations', [ReservationController::class, 'create']);
 Route::put('/reservations/{id}', [ReservationController::class, 'update']);
 Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
